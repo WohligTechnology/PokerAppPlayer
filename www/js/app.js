8718123407a172889cc7cb65d63f7c9eb992e0ec
Chintan <chintan@wohlig.com>
2017-12-03 13:10:37 +0530

    done

--- www/js/app.js
+++ www/js/app.js
@@ -26,22 +26,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     $ionicConfigProvider.views.maxCache(0);
     $stateProvider
 
-      .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
-      })
-
-      .state('app.search', {
-        url: '/search',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/search.html'
-          }
-        }
-      })
-
       .state('player', {
         url: '/player',
         templateUrl: 'templates/player.html',
@@ -54,34 +38,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         url: '/tab',
         templateUrl: 'templates/tab.html',
         controller: 'TabCtrl'
-      })
-
-      .state('app.browse', {
-        url: '/browse',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/browse.html'
-          }
-        }
-      })
-      .state('app.playlists', {
-        url: '/playlists',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlists.html',
-            controller: 'PlaylistsCtrl'
-          }
-        }
-      })
-
-      .state('app.single', {
-        url: '/playlists/:playlistId',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/playlist.html',
-            controller: 'PlaylistCtrl'
-          }
-        }
       });
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/player');
